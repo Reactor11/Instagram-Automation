f54c4ae0c76074e41bcd1ddf40da8a6c4210460b
Manas Bhardwaj <manas1109bhardwaj@gmail.com>
2020-06-08 00:56:01 +0530

    Update AutoClicker.py

--- AutoClicker.py
+++ AutoClicker.py
@@ -34,6 +34,13 @@ def auto(rep,url):
             driver = webdriver.Chrome(loc, chrome_options=chrome_options)
             driver.get(url)
             time.sleep(np.random.randint(5,10))
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(np.random.randint(5,10))
+            case_study_path = '//*[@id="comp-k9y7oef73label"]'
+            driver.find_element_by_xpath(case_study_path).click()
+            time.sleep(np.random.randint(5,10))
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(np.random.randint(5,10))
             print("Successful click : ",i+1)
             print("auto assigned to thread: {}\n\n".format(threading.current_thread().name))
             driver.quit()
@@ -41,6 +48,40 @@ def auto(rep,url):
             print("ERROR : ",e)
             time.sleep(5)
 
+            
+def auto_mobile(rep,url):
+    for i in range(rep):
+        try:
+            mobile_emulation = {"deviceMetrics": { "width": 360, "height": 640, "pixelRatio": 3.0 },"userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"}
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument("--incognito")
+            chrome_options.add_argument("--headless")
+            chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+            PROXY = list(get_proxies())
+            if(len(PROXY) != 0):
+                chrome_options.add_argument("--window-size=1920x1080")
+                chrome_options.add_argument('--proxy-server=%s' % PROXY[np.random.randint(0,len(PROXY))])    
+            loc = os.path.abspath(os.getcwd()) + "/chromedriver"
+            driver = webdriver.Chrome(loc, chrome_options=chrome_options)
+            driver.get(url)
+            time.sleep(np.random.randint(5,10))
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(np.random.randint(5,10))
+            case_study_path = '//*[@id="comp-k9y7oef73label"]'
+            driver.find_element_by_xpath(case_study_path).click()
+            time.sleep(np.random.randint(5,10))
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(np.random.randint(5,10))
+            print("Successful click : ",i+1)
+            print("auto assigned to thread: {}\n\n".format(threading.current_thread().name))
+            driver.quit()
+        except Exception as e :
+            print("ERROR : ",e)
+            time.sleep(5)
+
+            
+            
+            
 url = str(sys.argv[1])
 rep = np.random.randint(1,10)
 
@@ -49,11 +90,11 @@ t2 = threading.Thread(target=auto, name='t2',args=(rep,url,))
 t3 = threading.Thread(target=auto, name='t3',args=(rep,url,))
 t4 = threading.Thread(target=auto, name='t4',args=(rep,url,))
 t5 = threading.Thread(target=auto, name='t5',args=(rep,url,))
-t6 = threading.Thread(target=auto, name='t6',args=(rep,url,))
-t7 = threading.Thread(target=auto, name='t7',args=(rep,url,))
-t8 = threading.Thread(target=auto, name='t8',args=(rep,url,))
-t9 = threading.Thread(target=auto, name='t9',args=(rep,url,))
-t10 = threading.Thread(target=auto, name='t10',args=(rep,url,))
+t6 = threading.Thread(target=auto_mobile, name='t6',args=(rep,url,))
+t7 = threading.Thread(target=auto_mobile, name='t7',args=(rep,url,))
+t8 = threading.Thread(target=auto_mobile, name='t8',args=(rep,url,))
+t9 = threading.Thread(target=auto_mobile, name='t9',args=(rep,url,))
+t10 = threading.Thread(target=auto_mobile, name='t10',args=(rep,url,))
 
 
 t1.start()
